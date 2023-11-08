22d8fbf3fa7d42bbf662182b509ecf9455a25dbd
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-08 09:56:40 +0700

    Slide_19

---
+++ PHP/3-3rdPHP.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo"Hà Tấn Tường -2121050164";
+    ?>
+    <h3>Hello World!</h3>
+    <select>
+        <option>1</option>
+        <option>2</option>
+    </select>
+
+</body>
+</html>
