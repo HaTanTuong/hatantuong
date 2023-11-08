11452d7054c4d9108f24dad9020d556e85560c6c
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-08 09:50:29 +0700

    Slide_13

---
+++ PHP/1-firstPHP.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Hà Tấn Tường - 2121050164";
+    echo "<h3>Hello World!</h3>";
+    echo "<select>";
+    echo "<option>1</option>";
+    echo "<option>2</option>";
+    echo "</select>";
+?>
