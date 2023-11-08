8f48c050e9a1ea0c394804a5e45c7deb03211c9c
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-08 09:52:45 +0700

    Slide_17

---
+++ PHP/2-2ndPHP.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hà Tấn Tường - 2121050164";
+        echo "<h3>Hello World!</h3>";
+        echo "<select>";
+        echo "<option>1</option>";
+        echo "<option>2</option>";
+        echo "</select>";
+    ?>
+</body>
+</html>
