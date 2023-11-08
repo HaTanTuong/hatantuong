43c2f71b4cde7bc888afdb5f738b73c1db3f994e
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-08 15:52:51 +0700

    Slide_30_Practice_03_DemKiTu

--- PHP/practice.php
+++ PHP/practice.php
@@ -1,19 +1,9 @@
 <?php
     echo"<h2>Hà Tấn Tường -2121050164</h2>";
-    $a = 1;
-    $b = 6;
-    $c = 4;
-    
-    $delta = $b * $b - 4 * $a * $c;
-    echo "Phương trình: $a.x**2 + $b.x + $c"."<br>";
-    echo"Delta =".$delta."<br>";
-    
-    if ($delta > 0) {
-        echo "Phương trình có hai nghiệm phân biệt";
-    } elseif ($delta == 0) {
-        echo "Phương trình có một nghiệm kép";
-    } else {
-        echo "Phương trình không có nghiệm thực";
-    }
+    $chuoi = "chung tôi là sinhviên trường đại học mỏ địa chất";
+    $so_lan_h_xuat_hien = substr_count($chuoi, 'h');
+
+    echo "Số lần 'h' xuất hiện trong chuỗi: " . $so_lan_h_xuat_hien;
 ?>
+
     
