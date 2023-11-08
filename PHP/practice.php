7506cfe574edaac3fe7e0c21728cd724688b40f7
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-08 15:40:10 +0700

    Slide_30_Practice_02_NghiemCuaPT

--- PHP/practice.php
+++ PHP/practice.php
@@ -1,13 +1,19 @@
 <?php
-    echo"<h3>Tìm số lớn hơn</h3>";
-    $firstNumber = 3000;
-    $secondNumber = 1000;
-    if ($firstNumber > $secondNumber) 
-    {
-        echo "Sớ lớn nhất là ". $firstNumber;
-    }
-    else{
-        echo "Sớ lớn nhất là ". $secondNumber;
-
+    echo"<h2>Hà Tấn Tường -2121050164</h2>";
+    $a = 1;
+    $b = 6;
+    $c = 4;
+    
+    $delta = $b * $b - 4 * $a * $c;
+    echo "Phương trình: $a.x**2 + $b.x + $c"."<br>";
+    echo"Delta =".$delta."<br>";
+    
+    if ($delta > 0) {
+        echo "Phương trình có hai nghiệm phân biệt";
+    } elseif ($delta == 0) {
+        echo "Phương trình có một nghiệm kép";
+    } else {
+        echo "Phương trình không có nghiệm thực";
     }
 ?>
+    
