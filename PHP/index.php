8440fb10f84ba4979d0730eef1cd67c1ed4adee5
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-08 10:16:09 +0700

    Slide_25

--- PHP/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $tuongAccount = 10000000;
-    echo "So tien trong tai khoan Long: $tuongAccount";
-    $HUMGPay = 10000000;
-    echo "HUMG chuyen tien: SHUMGPay", $tuongAccount = $tuongAccount + $HUMGPay;
-    echo "So tien trong tai khoan Ha Tan Tuong la: $tuongAccount";
-?>
