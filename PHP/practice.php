a0acf921b86982354732609e689396c22c337750
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-08 15:32:05 +0700

    Slide_30_Practice_TimSoLonHon

---
+++ PHP/practice.php
@@ -0,0 +1,13 @@
+<?php
+    echo"<h3>Tìm số lớn hơn</h3>";
+    $firstNumber = 3000;
+    $secondNumber = 1000;
+    if ($firstNumber > $secondNumber) 
+    {
+        echo "Sớ lớn nhất là ". $firstNumber;
+    }
+    else{
+        echo "Sớ lớn nhất là ". $secondNumber;
+
+    }
+?>
