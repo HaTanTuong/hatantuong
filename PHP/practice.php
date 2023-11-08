66c121c4e9e6a0ec16ab476fc54bf873a9a339c6
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-08 15:59:08 +0700

    Slide_30_Practice_04_KiemTraTuGiac

--- PHP/practice.php
+++ PHP/practice.php
@@ -1,9 +1,13 @@
 <?php
-    echo"<h2>Hà Tấn Tường -2121050164</h2>";
-    $chuoi = "chung tôi là sinhviên trường đại học mỏ địa chất";
-    $so_lan_h_xuat_hien = substr_count($chuoi, 'h');
+    echo"<h2>Hà Tấn Tường - 2121050164</h2>";
+    $canh1 =    10;
+    $canh2 =    18;
+    echo "chiều dài cạnh thứ nhất là:". $canh1. "<br>";
+    echo "chiều dài cạnh thứ hai là:". $canh2. "<br>";
 
-    echo "Số lần 'h' xuất hiện trong chuỗi: " . $so_lan_h_xuat_hien;
+    if ($canh1 == $canh2) {
+        echo "Tứ giác là hình vuông";
+    } else {
+        echo "Tứ giác là hình chữ nhật";
+    }
 ?>
-
-    
