28beb57ee6066ff56e87fe01cb6c06d1826b6795
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-13 19:49:31 +0700

    Slide_48

---
+++ PHP/7-CalResult.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hà Tấn Tường - 2121050164</title>
+</head>
+<body>
+    <?php
+        $result = $_GET["firstNumber"] + $_GET["secondNumber"];
+        echo $result;
+    ?>
+</body>
+</html>
