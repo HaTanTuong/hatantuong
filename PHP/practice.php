e684770a681201fa694e589c9cee8fa6d856522e
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-20 11:39:53 +0700

    Slide_22_BTVN_1

--- PHP/practice.php
+++ PHP/practice.php
@@ -1,44 +1,34 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
-    <body>
-        <h3>Hà Tấn Tường</h3>
-            <form action="" method="get">
-                Math: <input type="number" name="math"><br/>
-                Physics: <input type="number" name="physics"><br/>
-                Chemistry: <input type="number" name="chemistry"><br/>
-                <input type="submit" value="Xét">
-            </form>
-        <?php
-            $math = $_GET["math"];
-            $physics = $_GET["physics"];
-            $chemistry = $_GET["chemistry"];
-            $Sum =  ($math + $physics + $chemistry);
-            echo"Tổng điểm của bạn: $Sum </br>";
-
-            if ($Sum >= 22){
-                echo 'Bạn có thể trúng tuyển ngành: CNTT, CNTT chất lượng cao, KHDL, Địa chất, Môi trường';
-            }
-            else if ($Sum>=18 && $Sum <22){
-                echo 'Bạn có thể trúng tuyển ngành: CNTT, KHDL, Địa chất, Môi trường';
-
-            }
-            else if ($Sum>=17 && $Sum <18){
-                echo 'Bạn có thể trúng tuyển ngành: Địa chất, Môi trường';
-
-            }
-            else if ($Sum>= 15&& $Sum < 17){
-                echo 'Bạn có thể trúng tuyển ngành: Môi trường';
-            }
-            else{
-                echo 'Bạn không trúng tuyển ngành nào';
-            }
-
-
-
-            
-        ?>
-
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Is It Christmas?</title>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+        }
+        h1 {
+            text-align: center;
+            font-size: 150px;
+            padding-top: 140px;
+            font-weight: 500;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $day = date("jS");
+        $month = date("F");
+        if ($day == "25th" AND $month == "December")
+        {
+            echo "<h1>ĐÚNG</h1>";
+        }
+        else
+        {
+            echo "<h1>SAI</h1>";
+        }
+    ?>    
+</body>
+</html>
