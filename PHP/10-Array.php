abedeec57be30e375090dbc126a1bd20457dbdc9
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-22 16:01:32 +0700

    Slide_26

---
+++ PHP/10-Array.php
@@ -0,0 +1,13 @@
+<?php
+    $exchangeRate = array (22308, 27380, 17008, 128);
+    $exchangeRate[] = 6000;
+    $exchangeRate[] = 16000;
+    $exchangeRate[0] = 22200;
+    echo "<h3>Exchange rate to VND</h3>";
+    echo "USD: $exchangeRate[0]<br>";
+    echo "EUR: $exchangeRate[1]<br>";
+    echo "SGD: $exchangeRate[2]<br>";
+    echo "JPY: $exchangeRate[3]<br>";
+    echo "CHN: $exchangeRate[4]<br>";
+    echo "AUD: $exchangeRate[5]<br>";
+?>
