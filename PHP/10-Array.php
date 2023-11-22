f67363f985ecb87324e0a9855366cddb207e2fbb
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-22 16:06:23 +0700

    Slide_30

--- PHP/10-Array.php
+++ PHP/10-Array.php
@@ -1,13 +1,8 @@
 <?php
-    $exchangeRate = array (22308, 27380, 17008, 128);
-    $exchangeRate[] = 6000;
-    $exchangeRate[] = 16000;
-    $exchangeRate[0] = 22200;
+    $exchangeRate = array ("USD"=>22300, "EUR"=> 27300, "SGD"=>17000, "JPY"=>120);
     echo "<h3>Exchange rate to VND</h3>";
-    echo "USD: $exchangeRate[0]<br>";
-    echo "EUR: $exchangeRate[1]<br>";
-    echo "SGD: $exchangeRate[2]<br>";
-    echo "JPY: $exchangeRate[3]<br>";
-    echo "CHN: $exchangeRate[4]<br>";
-    echo "AUD: $exchangeRate[5]<br>";
+    echo "USD:". $exchangeRate['USD']."<br>";
+    echo "EUR:". $exchangeRate['EUR']."<br>";
+    echo "SGD:". $exchangeRate['SGD']."<br>";
+    echo "JPY:". $exchangeRate['JPY']."<br>";
 ?>
