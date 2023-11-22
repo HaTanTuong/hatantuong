4afa01988e1450f3609b6dcf742ea4d4ffa258d3
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-22 16:46:56 +0700

    Slide_35_PHP_BTVN2

--- PHP/9-ifPracticeResult.php
+++ PHP/9-ifPracticeResult.php
@@ -6,10 +6,9 @@
 </head>
 <body>
     <?php
-        $exchangeRate = array("USD"=>22380, "EUR"=>27300, "SGD">17080, "JPY"=>128);
+        $exchangeRate = array("USD"=>22300, "EUR"=>27300, "SGD">17000, "JPY"=>120);
         $amount = $_GET['amount'];
         $currency = $_GET["currency"];
-
         echo " Samount USD is equal". number_format($amount* $exchangeRate[$currency])."VNĐ";
     ?>
 </body>
--- PHP/practice.php
+++ PHP/practice.php
@@ -1,49 +1,50 @@
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title>Hà Tấn Tường - 2121050164</title>
-    
+    <title>Document</title>
 </head>
+
 <body>
-<h1>Kiểm Tra Tuyển Dụng</h1>
-    <form action="" method="get">
-        <div>
-            <label>Giới tính:</label>
-            <select name ="gioitinh">
-            <option value="nam">Nam</option>
-            <option value="nu">Nữ</option>
-            </select>
-        </div>
-        <div>
-            <label>Chiều cao(m):</label>
-            <input type="number" step="0.01" name="chieucao">
-            <!--step: để chỉ định bước nhảy cho các giá trị được chọn hoặc nhập vào trường số.-->
-        </div>
-        <div>
-            <label>Cân nặng(kg):</label>
-            <input type="number" step="0.1" name="cannang">
-        </div>
-        <button type="submit">Check</button>
-    </form>
-  
     <?php
-        $gioitinh = $_GET['gioitinh'];
-        $chieucao = $_GET['chieucao'];
-        $cannang = $_GET['cannang'];
-      
-        if ($gioitinh == 'nam') {
-          if ($chieucao > 1.6 && $cannang > 50) {
-            echo'Xin chúc mừng! Bản đáp ứng đủ điều kiện để gia nhập công ty.';
-          } else {
-            echo 'Xin lỗi! Bạn không đủ điều kiện gia nhập công ty.';
-          }
-        } else if ($gioitinh == 'nu') {
-          if ($chieucao > 1.5 && $cannang > 40 && $cannang < 70) {
-            echo 'Xin chúc mừng! Bản đáp ứng đủ điều kiện để gia nhập công ty.';
-          } else {
-            echo 'Xin lỗi! Bạn không đủ điều kiện gia nhập công ty.';
-          }
+        $math = $_GET["math"];
+        $physics = $_GET["physics"];
+        $chemistry = $_GET["chemistry"];
+        $english = $_GET["english"];
+        $literature = $_GET["literature"];
+        $history = $_GET["history"];
+        $Mean =  ($math + $physics + $chemistry + $english + $literature + $history) / 6;
+        echo "Điểm trung bình của bạn: $Mean </br>";
+
+        if ($math < 0 || $math > 10) {
+            echo 'Điểm môn Toán phải từ 0 -> 10';
+        } else if ($physics < 0 || $physics > 10) {
+            echo 'Điểm môn Lý phải từ 0 -> 10';
+        } else if ($chemistry < 0 || $chemistry > 10) {
+            echo 'Điểm môn Hóa phải từ 0 -> 10';
+        } else if ($english < 0 || $english > 10) {
+            echo 'Điểm môn Anh Văn phải từ 0 -> 10';
+        } else if ($literature < 0 || $literature > 10) {
+            echo 'Điểm môn Văn phải từ 0 -> 10';
+        } else if ($history < 0 || $history > 10) {
+            echo 'Điểm môn Lịch Sử phải từ 0 -> 10';
+        } else if ($math <= 4 || $physics <= 4 || $chemistry <= 4 || $english <=  4 || $literature <= 4 || $history <= 4) {
+            echo 'Học sinh giỏi yếu';
+        } else {
+            $Mean = ($math + $physics + $chemistry + $english + $literature + $history) / 6;
+
+            if ($Mean < 5) {
+                echo "Học sinh yếu";
+            } else if ($Mean >= 5 && $Mean < 6.4) {
+                echo "Học sinh trung bình";
+            } else if ($Mean >= 6.4 && $Mean < 7.9) {
+                echo "Học sinh khá";
+            } else {
+                echo "Học sinh giỏi";
+            }
         }
     ?>
 </body>
+
 </html>
