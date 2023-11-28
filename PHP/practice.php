aa032110103cfe3efea68ffb6a30277561701b7c
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-28 19:17:52 +0700

    Slide_23

--- PHP/practice.php
+++ PHP/practice.php
@@ -6,14 +6,8 @@
             echo "<img src='images/$item' width=50px height = 50px alt='$item'>";
         }
     }
-    $items = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
-        dislay_image_list($items);
-?>
+    ?>
 
 
 <h1>function</h1>
 <h2>Hà Tấn Tường - 2121050164</h2>
-<?php
-    $items = array('item5.png', 'item6.png');
-    dislay_image_list($items);
-?>
