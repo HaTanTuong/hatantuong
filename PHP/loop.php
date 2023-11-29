c2e26127fb6323dfe3d68e1c42e9d7298682d463
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-29 15:17:23 +0700

    Slide_14_ForeachLoop

--- PHP/loop.php
+++ PHP/loop.php
@@ -1,6 +1,7 @@
 <?php 
-    $x = 1;
-    while($x <=10){
-        echo"Đây là số: $x <br>";   
+    $items = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
+    foreach ($items as $item)
+    {
+        echo "<img src='images/$item' width=50px height = 50px alt='$item'>";
     }
 ?>
