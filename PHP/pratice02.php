5c3ec589636c89ee629e400e5d023f78bec737a4
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-29 15:11:31 +0700

    Slide_3_LOOPS

--- PHP/pratice02.php
+++ PHP/pratice02.php
@@ -1,5 +1,29 @@
-<?php
-    require('practice.php');
-    $items = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
-    dislay_image_list($items);
-?>
+Ngày:
+<select>
+    <?php
+        for ($day=1; $day<= 31 ; $day++) 
+        { 
+            echo "<option value = '$day'> $day </option>";
+        }
+    ?>
+</select>
+Tháng:
+<select>
+    <?php
+        for ($month=1; $month <= 12 ; $month++) 
+        { 
+            echo "<option value = '$month'>$month</option>";
+        }
+    ?>
+</select>
+Năm:
+<select>
+    <?php
+        $today = getdate();
+        $curYear = $today["year"];
+        for ($year=1900;  $year <=$curYear ; $year+=1) 
+        { 
+            echo "<option value = '$year'>$year</option>";
+        }
+    ?>
+</select>
