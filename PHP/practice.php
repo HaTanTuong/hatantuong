3dff0f7ab35c62f2995f5d9cfbf41bff4884ee48
Hà Tấn Tường <hatuong092002@gmail.com>
2023-11-29 10:39:50 +0700

    Slide_25_BTVN4

--- PHP/practice.php
+++ PHP/practice.php
@@ -1,13 +1,55 @@
 <?php
-    function dislay_image_list($imagelist)
-    {
-        foreach ($imagelist as $item)
-        {
-            echo "<img src='images/$item' width=50px height = 50px alt='$item'>";
-        }
+// Hàm tính điểm trung bình của học sinh từ các môn học
+function calculateAverage($math, $physics, $chemistry, $english, $literature, $history) {
+    // Tạo một mảng chứa điểm từng môn học
+    $grades = array($math, $physics, $chemistry, $english, $literature, $history);
+    
+    // Số lượng môn học
+    $totalSubjects = count($grades);
+    
+    // Tính tổng điểm của các môn học
+    $sum = array_sum($grades);
+    
+    // Tính điểm trung bình
+    $average = $sum / $totalSubjects;
+    
+    // Trả về giá trị điểm trung bình
+    return $average;
+}
+
+// Hàm xác định xếp loại học sinh dựa trên điểm trung bình
+function classifyStudent($average) {
+    if ($average < 5) {
+        return "Học sinh yếu";
+    } elseif ($average >= 5 && $average < 6.5) {
+        return "Học sinh trung bình";
+    } elseif ($average >= 6.5 && $average < 8) {
+        return "Học sinh khá";
+    } else {
+        return "Học sinh giỏi";
     }
-    ?>
+}
 
+// Kiểm tra nếu request method là POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Lấy điểm từ form POST
+    $math = $_POST["math"];
+    $physics = $_POST["physics"];
+    $chemistry = $_POST["chemistry"];
+    $english = $_POST["english"];
+    $literature = $_POST["literature"];
+    $history = $_POST["history"];
 
-<h1>function</h1>
-<h2>Hà Tấn Tường - 2121050164</h2>
+    // Tính điểm trung bình
+    $average = calculateAverage($math, $physics, $chemistry, $english, $literature, $history);
+
+    // Hiển thị kết quả điểm trung bình
+    echo "<h3>Điểm trung bình:</h3>";
+    echo "<p>Điểm trung bình của bạn là: " . number_format($average, 2) . "</p>";
+
+    // Xác định và hiển thị xếp loại học sinh
+    echo "<h3>Xếp loại:</h3>";
+    $classification = classifyStudent($average);
+    echo $classification;
+}
+?>
---
+++ PHP/pratice02.php
@@ -0,0 +1,5 @@
+<?php
+    require('practice.php');
+    $items = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
+    dislay_image_list($items);
+?>
